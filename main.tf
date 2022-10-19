59163a281db5d10d5c4f1bf1420274a2414a6e37
Sandhya-571 <97505070+Sandhya-571@users.noreply.github.com>
2022-10-19 11:45:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ provider "aws"{
 Create the VPC
  resource "aws_vpc" "Main" {                # Creating VPC here
    cidr_block       = var.main_vpc_cidr     # Defining the CIDR block use 10.0.0.0/24 for demo
+   name = "task3"
    instance_tenancy = "default"
  }
  Create Internet Gateway and attach it to VPC
