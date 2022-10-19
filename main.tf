73874b9cdfbf46a9a4ad7a8bc8d8ce4def2a35c9
Sandhya-571 <97505070+Sandhya-571@users.noreply.github.com>
2022-10-19 10:38:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.31.0"
+    }
+  }  
+  }
+provider "aws"{
+  region = "us-east-2"
+}
 Create the VPC
  resource "aws_vpc" "Main" {                # Creating VPC here
    cidr_block       = var.main_vpc_cidr     # Defining the CIDR block use 10.0.0.0/24 for demo
